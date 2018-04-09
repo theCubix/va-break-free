d653388e74786dcff16d054c81c755d20d5e0577
[thecubix] <[timon.forrer@gmail.com]>
2018-04-10 00:26:17 +0200

    Added background

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import './index.scss'
+
 class LandingPage extends React.Component {
   render() {
     const image = this.props.image
 
     return (
-      <div>Works {image.src}</div>
+      <section className="landingpage-section">
+        <div className="landingpage-cover"></div>
+        <div
+          style={{ backgroundImage: `url(${image.src})` }}
+          className="landingpage-background"
+        ></div>
+      </section>
     )
   }
 };
