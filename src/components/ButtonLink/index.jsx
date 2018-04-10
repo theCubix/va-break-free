3ec2fdeae18cc8fc26229353bff88a2edd2ec33e
[thecubix] <[timon.forrer@gmail.com]>
2018-04-10 19:57:32 +0200

    Completed the ButtonLink component

--- src/components/ButtonLink/index.jsx
+++ src/components/ButtonLink/index.jsx
@@ -6,9 +6,13 @@ class ButtonLink extends React.Component {
   render() {
     const linkTo = this.props.linkTo;
     const title = this.props.linkTitle;
+    const classNameProp = this.props.className;
 
     return (
-      <a href={linkTo} className="btn-link">{title}</a>
+      <a
+        href={linkTo}
+        className={`btn-link ${classNameProp}`}
+      >{title}</a>
     )
   }
 };
