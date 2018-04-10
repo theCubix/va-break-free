449e450471935bfda137f71b9f13dbcf81ad54cd
[thecubix] <[timon.forrer@gmail.com]>
2018-04-10 20:03:40 +0200

    Added Section

---
+++ src/components/SectionAppleMusicEmbed/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.scss';
+
+class SectionAppleMusicEmbedd extends React.Component {
+  render() {
+    return (
+      <section className="music-embedd-section">
+        <h1>Music</h1>
+      </section>
+    )
+  }
+};
+
+export default SectionAppleMusicEmbedd;
