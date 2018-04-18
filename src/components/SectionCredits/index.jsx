1821ab320aae5c9acdc30cb3a966c34f78f6fd31
[thecubix] <[timon.forrer@gmail.com]>
2018-04-18 21:17:03 +0200

    Rebuilt site

--- src/components/SectionCredits/index.jsx
+++ src/components/SectionCredits/index.jsx
@@ -12,7 +12,7 @@ class SectionCredits extends React.Component {
         <div className="container">
           <h2 className="title">Danksagung</h2>
           <p>Ohne die Unterstützung und Mithilfe, die wir in den letzten Monaten erhalten haben, wäre unsere EP nie Realität geworden.</p>
-          <p>Ein grosses Dankeschön geht daher an Daniel Maurer und Rapper <OutboundLink href="https://www.facebook.com/che.b.offiziell/">Che-B</OutboundLink>, die im Studio für die Abmischung, den Schnitt sowie das Mixing verantwortlich waren und uns im allgemeinen mit ihrem Know-How unterstützt haben.</p>
+          <p>Ein grosses Dankeschön geht daher an Daniel Maurer und Rapper <OutboundLink href="https://www.facebook.com/che.b.offiziell/">Che-B</OutboundLink>, die im Studio für die Abmischung, den Schnitt sowie das Mixing verantwortlich waren und uns im allgemeinen mit ihrem Know-How unterstützten.</p>
           <p>Ebenfalls möchten wir <OutboundLink href="https://www.facebook.com/mury0/">Caroline Laville</OutboundLink> danken, welche das Motif für das Cover entworfen und umgesetzt hat.</p>
           <p>Das Schwyzer-Örgeli-Intro in «Break Free» wurde Komponiert von Toni Bürgler. Auch ihm möchten wir ein herzliches Dankeschön aussprechen.</p>
         </div>
